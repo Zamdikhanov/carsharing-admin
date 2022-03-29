ff3970041a77a57bf2a8c4acfee609aac7b0f84a
Zamdikhanov <oxothuk.sky@mail.ru>
2022-03-29 22:54:49 +0400

    add styles

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,23 +1,30 @@
 import css from "./LoginPage.module.scss";
+import { ReactComponent as Logo } from '../../assets/logo.svg';
 
 function LoginPage() {
   return (
     <div className={css.container}>
       <div className={css.block}>
-        <header className={css.block_header}>Need for drive</header>
+        <header className={css.block_header}>
+          <Logo />
+          <h2 className={css.block_header__title}>Need for drive</h2>
+        </header>
         <main className={css.card}>
           <h3 className={css.card_header}>Вход</h3>
-          <label className={css.input_label}>
-            <span>Почта</span>
-            <input type="text" placeholder="admin@ss.com" />
+          <div className={css.input_container}>
+            <label>Почта</label>
+            <input type="email" placeholder="admin@ss.com" />
             <div>error</div>
-          </label>
-          <label className={css.input_label}>
-            <span>Пароль</span>
+          </div>
+          <div className={css.input_container}>
+            <label>Пароль</label>
             <input type="password" placeholder="пароль*" />
             <div>error</div>
-          </label>
-          <div>button</div>
+          </div>
+          <div className={css.card_footer}>
+            <button className={css.button}>Запросить доступ</button>
+            <button className={css.standart_button}>Войти</button>
+          </div>
         </main>
       </div>
     </div>
