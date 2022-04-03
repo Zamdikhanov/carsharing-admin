4c28a965ac6572534d57a8ef27059cf1316a0e45
Zamdikhanov <93732183+Zamdikhanov@users.noreply.github.com>
2022-04-03 20:16:01 +0400

    V-08 header in progress

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,23 @@
 import css from "./Header.module.scss";
+import avatar from "../../assets/header/user-avatar.jpg";
+import { ReactComponent as Dropdown } from "./../../assets/header/dropdown-icon.svg";
+import { ReactComponent as Notifications } from "./../../assets/header/notifications.svg";
+
 function Header() {
   return (
     <header className={css.header}>
-      <input className={css.search} />
-      <div className={css.notifications}>mes</div>
-      <div className={css.user_details}>login</div>
+      <input className={css.search} placeholder="Поиск …" />
+      <div className={css.notifications}>
+        <Notifications className={css.notifications__svg} />
+        <div className={css.notifications__count}>2</div>
+      </div>
+      <div className={css.user_details}>
+        <img className={css.user_details__image} src={avatar} alt="avatar" />
+        <div className={css.user_details__name}>Admin</div>
+        <button className={css.user_details__dropdown}>
+          <Dropdown className={css.user_details__dropdown_svg} />
+        </button>
+      </div>
     </header>
   );
 }
