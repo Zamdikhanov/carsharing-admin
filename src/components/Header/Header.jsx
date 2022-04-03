ff09e20ed35694203c131e653a4e72dcbf24f034
Zamdikhanov <oxothuk.sky@mail.ru>
2022-04-03 22:52:07 +0400

    V-08 finish header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,27 @@
 import css from "./Header.module.scss";
 import avatar from "../../assets/header/user-avatar.jpg";
-import { ReactComponent as Dropdown } from "./../../assets/header/dropdown-icon.svg";
-import { ReactComponent as Notifications } from "./../../assets/header/notifications.svg";
+import { ReactComponent as DropdownSvg } from "./../../assets/header/dropdown-icon.svg";
+import { ReactComponent as NotificationsSvg } from "./../../assets/header/notifications.svg";
+import { ReactComponent as SearchSvg } from "./../../assets/header/search-icon.svg";
 
 function Header() {
   return (
     <header className={css.header}>
-      <input className={css.search} placeholder="Поиск …" />
-      <div className={css.notifications}>
-        <Notifications className={css.notifications__svg} />
+      <label className={css.search_container}>
+        <SearchSvg className={css.search_image} />
+        <input className={css.search} placeholder="Поиск …" type='search' />
+      </label>
+      <button className={css.notifications}>
+        <NotificationsSvg className={css.notifications__svg} />
         <div className={css.notifications__count}>2</div>
-      </div>
-      <div className={css.user_details}>
+      </button>
+      <button className={css.user_details}>
         <img className={css.user_details__image} src={avatar} alt="avatar" />
         <div className={css.user_details__name}>Admin</div>
-        <button className={css.user_details__dropdown}>
-          <Dropdown className={css.user_details__dropdown_svg} />
-        </button>
-      </div>
+        <div className={css.user_details__dropdown}>
+          <DropdownSvg className={css.user_details__dropdown_svg} />
+        </div>
+      </button>
     </header>
   );
 }
