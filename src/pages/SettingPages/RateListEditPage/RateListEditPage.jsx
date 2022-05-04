c7639edcf588986a4ea5c2e3077c1eea2f687f97
Zamdikhanov <oxothuk.sky@mail.ru>
2022-05-04 23:21:47 +0400

    F-09 edit RateListEditPage

--- src/pages/SettingPages/RateListEditPage/RateListEditPage.jsx
+++ src/pages/SettingPages/RateListEditPage/RateListEditPage.jsx
@@ -17,7 +17,7 @@ function RateListEditPage() {
         formState: { errors },
     } = useForm({ mode: 'onBlur' });
 
-    const onSubmit = () => {};
+    const onSubmit = () => { };
 
     return (
         <PageMainCard pageTitle="Добавление стоимости тарифа">
@@ -25,30 +25,39 @@ function RateListEditPage() {
             <PageMainCardMain>
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <div className={css.central_container}>
-                        <Select
-                            className={css.input}
-                            classNamePrefix={css.input}
-                            // {...item}
-                            // value={filterValue[index]}
-                            // components={{ DropdownIndicator }}
-                            onChange={() => {}}
-                            noOptionsMessage={({ inputValue }) =>
-                                inputValue ? 'не найдено' : 'не найдено'
-                            }
-                        />
-                        <Input
-                            label="Стоимость"
-                            type="number"
-                            placeholder="Введите стоимость тарифа"
-                            register={register}
-                            required
-                            setError={setError}
-                            minLength={{
-                                value: 1,
-                                message: 'Не менее 1-го символа',
-                            }}
-                            errors={errors}
-                        />
+                        <div className={css.element_container}>
+                            <Select
+                                className={css.input}
+                                classNamePrefix={css.input}
+                                // {...item}
+                                // value={filterValue[index]}
+                                // components={{ DropdownIndicator }}
+                                onChange={() => { }}
+                                noOptionsMessage={({ inputValue }) =>
+                                    inputValue ? 'не найдено' : 'не найдено'
+                                }
+                            />
+                        </div>
+                        <div className={css.element_container}>
+                            <Input
+                                styles={{ flex: '1 1 320px' }}
+                                label="Стоимость"
+                                type="number"
+                                placeholder="Введите стоимость тарифа"
+                                register={register}
+                                required
+                                setError={setError}
+                                minLength={{
+                                    value: 1,
+                                    message: 'Не менее 1-го символа',
+                                }}
+                                maxLength={{
+                                    value: 10,
+                                    message: 'Не более 10-и символов',
+                                }}
+                                errors={errors}
+                            />
+                        </div>
                         <div className={css.button_block}>
                             <button className={css.button} type="submit">
                                 Сохранить
@@ -60,7 +69,7 @@ function RateListEditPage() {
                     </div>
                 </form>
             </PageMainCardMain>
-        </PageMainCard>
+        </PageMainCard >
     );
 }
 
