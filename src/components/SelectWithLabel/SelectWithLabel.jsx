24d80946be15fed9ada9322c20b51a7868b8a370
Zamdikhanov <93732183+Zamdikhanov@users.noreply.github.com>
2022-05-05 21:24:44 +0400

    F-09 add errors message on RateListEditPage

---
+++ src/components/SelectWithLabel/SelectWithLabel.jsx
@@ -0,0 +1,49 @@
+import Select, { components } from 'react-select';
+import { ReactComponent as DropdownSvg } from '../../assets/icons/dropdown.svg';
+import css from './SelectWithLabel.module.scss';
+
+function DropdownIndicator(props) {
+    return (
+        <components.DropdownIndicator {...props}>
+            <DropdownSvg />
+        </components.DropdownIndicator>
+    );
+}
+
+function SelectWithLabel(props) {
+    const { label, placeholder, id, errors, options, type, field, onChange } =
+        props;
+
+    const classNameSelect = `${css.input} ${
+        errors && errors[label] && css.input_error
+    }`;
+
+    return (
+        <div className={css.input_container}>
+            <label htmlFor={id}>
+                <div className={css.label}>{label}</div>
+                <Select
+                    className={classNameSelect}
+                    classNamePrefix={css.input}
+                    components={{ DropdownIndicator }}
+                    placeholder={placeholder}
+                    options={options}
+                    id={id}
+                    onChange={onChange}
+                    type={type}
+                    noOptionsMessage={({ inputValue }) =>
+                        inputValue ? 'не найдено' : 'не найдено'
+                    }
+                    errors={errors}
+                    {...field}
+                    required
+                />
+            </label>
+            {errors && errors[label] && (
+                <div className={css.error}>{errors[label].message}</div>
+            )}
+        </div>
+    );
+}
+
+export default SelectWithLabel;
--- src/pages/SettingPages/RateListEditPage/RateListEditPage.jsx
+++ src/pages/SettingPages/RateListEditPage/RateListEditPage.jsx
@@ -1,46 +1,92 @@
 import { Link } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
-import Select from 'react-select';
+import { useEffect, useState } from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import rateTypeApi from '../../../api/rateTypeApi';
 import {
     PageMainCard,
-    // PageMainCardHeader,
     PageMainCardMain,
 } from '../../../components/PageMainCard/PageMainCard';
+import SelectWithLabel from '../../../components/SelectWithLabel/SelectWithLabel';
 import Input from '../../../components/Input/Input';
 import css from './RateListEditPage.module.scss';
 
 function RateListEditPage() {
+    const [options, setOptions] = useState([]);
+
+    useEffect(() => {
+        let response;
+        async function fetchData() {
+            response = await rateTypeApi.getRateType({ page: 0, limit: 0 });
+            const responseOptions = response.data.data.map((item) => ({
+                value: item.id,
+                label: item.name,
+            }));
+            setOptions(responseOptions);
+        }
+        fetchData();
+    }, []);
+
     const {
+        control,
         register,
         handleSubmit,
         setError,
+        clearErrors,
         formState: { errors },
-    } = useForm({ mode: 'onBlur' });
+    } = useForm({
+        mode: 'onBlur',
+        defaultValues: {
+            select: () => options[0],
+        },
+        rules: { required: true },
+    });
 
-    const onSubmit = () => { };
+    const resetError = () => {
+        console.log('reset clicked');
+        clearErrors('Тариф');
+    };
+
+    const onSubmit = (data) => {
+        if (!data['Тариф']?.value) {
+            setError(
+                'Тариф',
+                { type: 'select', message: 'Тариф не выбран' },
+                { shouldFocus: false },
+            );
+        } else {
+            const data2 = {
+                rateTypeId: data['Тариф'].value,
+                price: data['Стоимость'],
+            };
+            console.log('result', data2);
+        }
+    };
 
     return (
         <PageMainCard pageTitle="Добавление стоимости тарифа">
-            {/* <PageMainCardHeader>заголовок</PageMainCardHeader> */}
             <PageMainCardMain>
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <div className={css.central_container}>
                         <div className={css.element_container}>
-                            <Select
-                                className={css.input}
-                                classNamePrefix={css.input}
-                                // {...item}
-                                // value={filterValue[index]}
-                                // components={{ DropdownIndicator }}
-                                onChange={() => { }}
-                                noOptionsMessage={({ inputValue }) =>
-                                    inputValue ? 'не найдено' : 'не найдено'
-                                }
+                            <Controller
+                                name="Тариф"
+                                control={control}
+                                render={({ field }) => (
+                                    <SelectWithLabel
+                                        errors={errors}
+                                        field={field}
+                                        label="Тариф"
+                                        placeholder="Выберите тариф"
+                                        id="rateListEditPageSelet"
+                                        options={options}
+                                        required
+                                        onChange={resetError}
+                                    />
+                                )}
                             />
                         </div>
                         <div className={css.element_container}>
                             <Input
-                                styles={{ flex: '1 1 320px' }}
                                 label="Стоимость"
                                 type="number"
                                 placeholder="Введите стоимость тарифа"
@@ -69,7 +115,7 @@ function RateListEditPage() {
                     </div>
                 </form>
             </PageMainCardMain>
-        </PageMainCard >
+        </PageMainCard>
     );
 }
 
